a6205c27742c8400642af7b3ed1266e244654af6
way213 <108505154+way213@users.noreply.github.com>
2024-05-07 22:52:05 -0400

    Update grad_cam.py

--- grad_cam.py
+++ grad_cam.py
@@ -2,49 +2,37 @@ import tensorflow as tf
 import numpy as np
 
 
-def grad_cam(model, image_path, layer_name='conv5_block3_out', class_index=None):
-    img = tf.keras.preprocessing.image.load_img(
-        image_path, target_size=(64, 64))
+def grad_cam(model, image_path, layer_name='conv5_block3_out'):
+    img = tf.keras.preprocessing.image.load_img(image_path, target_size=(64, 64))
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
 
-    # Preprocess the input image
+    # Preprocess the image
     img_array = img_array / 255.0
 
-    # Generate class activation heatmap
-    heatmap_model = tf.keras.models.Model(
-        [model.inputs], [model.get_layer(layer_name).output, model.output])
-
+    # Compute gradient of the predicted category with respect to the given layer's output
     with tf.GradientTape() as tape:
-        last_conv_output, preds = heatmap_model(img_array)
-        if class_index is None:
-            class_index = tf.argmax(preds[0])
-        class_output = preds[:, class_index]
-
-    grads = tape.gradient(class_output, last_conv_output)
+        last_conv_layer_output, preds = model(img_array)
+        predicted_class = tf.argmax(preds[0])
+        class_output = preds[:, predicted_class]
+    grads = tape.gradient(class_output, last_conv_layer_output)
     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
 
-    heatmap = tf.reduce_mean(last_conv_output, axis=-1)
-    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    # Compute the heatmap
+    heatmap = tf.reduce_mean(last_conv_layer_output, axis=-1)
+    heatmap = tf.maximum(heatmap, 0)
+    heatmap /= tf.reduce_max(heatmap)
 
-    # Resize heatmap to the original image size
-    heatmap = tf.image.resize(
-        heatmap, (img_array.shape[1], img_array.shape[2]))
-    heatmap = tf.squeeze(heatmap)
+    heatmap = heatmap[0]
 
-    # Apply colormap
-    heatmap = tf.keras.utils.normalize(heatmap, axis=-1, order=1)
-    heatmap = tf.constant(heatmap)
-    heatmap = tf.expand_dims(heatmap, axis=-1)
-    heatmap = tf.image.grayscale_to_rgb(heatmap)
+    # Resize heatmap to the size of the original image
+    heatmap = cv2.resize(heatmap, (img_array.shape[2], img_array.shape[1]))
 
-    # Apply heatmap overlay
-    heatmap = tf.image.convert_image_dtype(heatmap, dtype=tf.uint8)
-    heatmap = tf.image.resize(heatmap, (64, 64))
+    # Convert heatmap to RGB
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
 
-    # Apply heatmap overlay on the original image
-    img_heatmap = tf.image.convert_image_dtype(img_array[0], dtype=tf.uint8)
-    img_heatmap = tf.image.resize(img_heatmap, (64, 64))
-    superimposed_img = tf.addWeighted(img_heatmap, 0.6, heatmap, 0.4, 0)
+    # Overlay heatmap on the original image
+    superimposed_img = cv2.addWeighted(img_array[0], 0.5, heatmap, 0.5, 0)
 
     return superimposed_img
